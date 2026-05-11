5fcfa49b491da0d873af72220a48d13690a5798a
Aurora1094 <2411094@mail.nankai.edu.cn>
2026-05-11 16:39:42 +0800

    keli跑通

---
+++ keil_compat/stm32f1xx.h
@@ -0,0 +1,18 @@
+#ifndef __STM32F1XX_H
+#define __STM32F1XX_H
+
+#ifndef STM32F10X_MD
+#define STM32F10X_MD
+#endif
+
+#include "stm32f10x.h"
+
+#ifndef RCC_CFGR_PPRE1_Pos
+#define RCC_CFGR_PPRE1_Pos 8U
+#endif
+
+#ifndef TIM_CCMR1_IC1F_Pos
+#define TIM_CCMR1_IC1F_Pos 4U
+#endif
+
+#endif
